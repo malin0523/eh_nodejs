b91cf6ec65f40b915a71f16544d5f64f555efa63
Beomjun Kim <malin0523@naver.com>
2023-01-17 14:53:11 +0900

    암호화 테스트 파일

--- test_d.js
+++ test_d.js
@@ -1,1 +1,8 @@
 const crypto = require('crypto');
+
+let origin = 'admin1234';
+let salt = 'mysalt1234';
+
+let result = crypto.pbkdf2Sync(origin, salt, 50, 255, 'sha512');
+
+console.log(result.toString('base64'));
